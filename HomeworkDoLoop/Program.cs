20014133f2c6217ff8347dd2d483f2a3182469b9
Taiwei Ko <152499488+macgeek39@users.noreply.github.com>
2024-01-04 06:55:45 -0600

    Initialized Commit

--- HomeworkDoLoop/Program.cs
+++ HomeworkDoLoop/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+/*
+ * Create a console application that asks the user 
+ * for their name. Welcome Tim as professor or
+ * anyone else as student. Do this until the user
+ * types "exit".
+ */
+
+// Ask the User for their name
+// If name is tim
+// Print "Welcome professor"
+// Or else "Welcome Student"
+// Until user type "exit" to exit the app
